9a814f2a1644e6b8cf836b8cb25c0512566aa084
dgilly2019 <dgilly2019@44-UC45-14.ad.campus-eni.fr>
2019-04-29 15:32:36 +0200

    Maj factory suppression imports

--- src/fr/eni/ecole/encheres/dal/DAOFactory.java
+++ src/fr/eni/ecole/encheres/dal/DAOFactory.java
@@ -8,14 +8,8 @@ import fr.eni.ecole.encheres.bo.Utilisateur;
 import fr.eni.ecole.encheres.dal.hibernate.ArticleDAOHibernate;
 import fr.eni.ecole.encheres.dal.hibernate.CategorieDAOHibernate;
 import fr.eni.ecole.encheres.dal.hibernate.EnchereDAOHibernate;
-import fr.eni.ecole.encheres.dal.hibernate.HibernateDAO;
 import fr.eni.ecole.encheres.dal.hibernate.RetraitDAOHibernate;
 import fr.eni.ecole.encheres.dal.hibernate.UtilisateurDAOHibernate;
-import fr.eni.ecole.encheres.dal.jdbc.ArticleDAOJdbcImpl;
-import fr.eni.ecole.encheres.dal.jdbc.CategorieDAOJdbcImpl;
-import fr.eni.ecole.encheres.dal.jdbc.EnchereDAOJdbcImpl;
-import fr.eni.ecole.encheres.dal.jdbc.RetraitDAOJdbcImpl;
-import fr.eni.ecole.encheres.dal.jdbc.UtilisateurDAOJdbcImpl;
 
 public class DAOFactory {
 	 public static <T> DAO getDAO(T t){
