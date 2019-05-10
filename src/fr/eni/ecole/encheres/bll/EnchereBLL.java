983589144f795ef32541c51d4cb0200cf091f93f
dgilly2019 <dgilly2019@44-UC45-14.ad.campus-eni.fr>
2019-05-10 12:09:01 +0200

    ajouts erreurs encheres

--- src/fr/eni/ecole/encheres/bll/EnchereBLL.java
+++ src/fr/eni/ecole/encheres/bll/EnchereBLL.java
@@ -91,24 +91,31 @@ public class EnchereBLL implements BLL<Enchere>{
 	@Override
 	public void set(Enchere enchere) throws BLLException, DALException {
 		// TODO Auto-generated method stub
+		if(enchere.getDateEnchere().after(enchere.getArticle().getDateFinEncheresArticle()) || enchere.getArticle().isTermine()) {
+			throw new BLLException(3000, "Cette vente est terminée");
+		}
+		
 		List<Enchere> encheres = this.getList(enchere.getArticle().getIdArticle());
 		if(encheres.size() != 0) {
 			for(Enchere oldEnchere : encheres) {
 				if(enchere.getMontantEnchere() <= oldEnchere.getMontantEnchere()) {
-					throw new BLLException(3000,"Il existe déjà une enchère plus elevée pour cet article");
+					throw new BLLException(3001,"Il existe déjà une enchère plus elevée pour cet article");
 				}
 			}
 			Enchere oldEnchere = this.get(enchere.getArticle().getIdArticle());
 			if(enchere.getArticle().getUtilisateurAchetant().getIdUtilisateur() == oldEnchere.getArticle().getUtilisateurAchetant().getIdUtilisateur()) {
-				throw new BLLException(3001, "Vous êtes déjà le meilleur enchérisseur");
+				throw new BLLException(3002, "Vous êtes déjà le meilleur enchérisseur");
+			}
+			if(enchere.getUtilisateur().getCreditUtilisateur() < enchere.getArticle().getPrixVenteArticle()) {
+				throw new BLLException(3003, "Vous ne disposez que de " + enchere.getUtilisateur().getCreditUtilisateur() + " crédits");
 			}
 		}
 		else {
 			if(enchere.getMontantEnchere() < enchere.getArticle().getPrixInitialArticle()) {
-				throw new BLLException(3002, "Impossible d'entrer une enchère inférieure à "+enchere.getArticle().getPrixInitialArticle());
+				throw new BLLException(3004, "Impossible d'entrer une enchère inférieure à "+enchere.getArticle().getPrixInitialArticle());
 			}
 			if(enchere.getDateEnchere().before(enchere.getArticle().getDateDebutEncheresArticle())) {
-				throw new BLLException(3003, "Le début des enchères n'est pas commencé");
+				throw new BLLException(3005, "Le début des enchères n'est pas commencé");
 			}
 		}
 		DAOFactory.getDAO(new Enchere()).insert(enchere);
